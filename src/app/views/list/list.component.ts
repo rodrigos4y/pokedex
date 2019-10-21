a480b3d949d53dc1d88fb2d4cd22792c089d5641
Rodrigo Dias de Freitas <rodrigodias@e-corp.acerp.br>
2019-10-21 11:47:09 -0300

    Utilizando Event e Property Binding para mostrar imagens do Pokemon

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
+  selectedPkm = null;
   pokemonList = [
     {name: 'Bulbasaur', number: 1},
     {name: 'Charmander', number: 4},
@@ -14,9 +14,18 @@ export class ListComponent implements OnInit {
     {name: 'Pikachu', number: 25}
   ]
 
+  get pkmSprite(){
+    const number = ('000' + this.selectedPkm.number).slice(-3);
+    return '//serebii.net/sunmoon/pokemon/'+ number +'.png';
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectPokemon(pkm){
+    this.selectedPkm = pkm;
   }
 
 }
